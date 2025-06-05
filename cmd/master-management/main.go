e348315a0fdd2b13ccfc2aabcb0a9acd9c2868a8
Sojo C Johny <sojocjohny37@gmail.com>
2025-06-05 14:23:29 +0530

    initiated database connection and migrations with a user model.

--- cmd/master-management/main.go
+++ cmd/master-management/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"master-management-api/cmd/config"
+	"master-management-api/internal/db"
+	"master-management-api/internal/models"
+)
 
+func init() {
+	config.LoadEnv()
+	db.Connect()
+}
+
+func main() {
+	db.DB.AutoMigrate(&models.User{})
 }
---
+++ internal/db/connection.go
@@ -0,0 +1,25 @@
+package db
+
+import (
+	"os"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var (
+	DB *gorm.DB
+)
+
+func Connect() {
+	db, err := gorm.Open(postgres.New(postgres.Config{
+		DSN:                  os.Getenv("POSTGRES_DSN"), // e.g., "host=localhost user=gorm password=gorm dbname=gorm port=9920"
+		PreferSimpleProtocol: true,                      // disables implicit prepared statement usage
+	}), &gorm.Config{})
+
+	if err != nil {
+		panic("failed to connect to database: " + err.Error())
+	}
+
+	DB = db
+}
