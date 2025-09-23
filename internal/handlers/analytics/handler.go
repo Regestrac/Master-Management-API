f66e6afa6b4bb412b2364a31cbd2c2af38580e9c
Sojo C Johny <sojocjohny37@gmail.com>
2025-09-23 14:43:15 +0530

    Added support for all time data fetching of quick metrics for analytics.

--- internal/handlers/analytics/handler.go
+++ internal/handlers/analytics/handler.go
@@ -4,6 +4,7 @@ import (
 	"master-management-api/internal/db"
 	"master-management-api/internal/models"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,6 +12,14 @@ import (
 func GetQuickMetrics(c *gin.Context) {
 	startDate := c.Query("startDate")
 	endDate := c.Query("endDate")
+	if endDate != "" {
+		t, err := time.Parse("02-01-2006", endDate)
+		if err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid date format"})
+			return
+		}
+		endDate = t.AddDate(0, 0, 1).Format("2006-01-02")
+	}
 	prevStartDate := c.Query("prevStartDate")
 	prevEndDate := c.Query("prevEndDate")
 
@@ -22,44 +31,61 @@ func GetQuickMetrics(c *gin.Context) {
 	var tasksCompleted int64
 	var tasksCompletedChange *int64 = nil
 
-	if err := db.DB.Model(&models.TaskSession{}).
-		Where("user_id = ? AND created_at BETWEEN ? AND ?", userId, startDate, endDate).
-		Select("COALESCE(SUM(duration),0)").
-		Scan(&totalFocusTime).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get focus time!"})
-		return
-	}
-	if prevStartDate != "" && prevEndDate != "" {
-		var prevFocusTime int64
+	if startDate != "" && endDate != "" {
 		if err := db.DB.Model(&models.TaskSession{}).
-			Where("user_id = ? AND created_at BETWEEN ? AND ?", userId, prevStartDate, prevEndDate).
+			Where("user_id = ? AND created_at BETWEEN ? AND ?", userId, startDate, endDate).
 			Select("COALESCE(SUM(duration),0)").
-			Scan(&prevFocusTime).Error; err != nil {
+			Scan(&totalFocusTime).Error; err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get focus time!"})
 			return
 		}
-		change := totalFocusTime - prevFocusTime
-		focusTimeChange = &change
-	}
+		if prevStartDate != "" && prevEndDate != "" {
+			var prevFocusTime int64
+			if err := db.DB.Model(&models.TaskSession{}).
+				Where("user_id = ? AND created_at BETWEEN ? AND ?", userId, prevStartDate, prevEndDate).
+				Select("COALESCE(SUM(duration),0)").
+				Scan(&prevFocusTime).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get focus time!"})
+				return
+			}
+			change := totalFocusTime - prevFocusTime
+			focusTimeChange = &change
+		}
 
-	if err := db.DB.Model(&models.Task{}).
-		Select("COUNT(*)").
-		Where("user_id = ? AND completed_at BETWEEN ? AND ?", userId, startDate, endDate).
-		Scan(&tasksCompleted).Error; err != nil {
-		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get completed tasks!"})
-		return
-	}
-	if prevStartDate != "" && prevEndDate != "" {
-		var prevTasksCompleted int64
 		if err := db.DB.Model(&models.Task{}).
 			Select("COUNT(*)").
-			Where("user_id = ? AND completed_at BETWEEN ? AND ?", userId, prevStartDate, prevEndDate).
-			Scan(&prevTasksCompleted).Error; err != nil {
+			Where("user_id = ? AND completed_at BETWEEN ? AND ?", userId, startDate, endDate).
+			Scan(&tasksCompleted).Error; err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get completed tasks!"})
 			return
 		}
-		change := tasksCompleted - prevTasksCompleted
-		tasksCompletedChange = &change
+		if prevStartDate != "" && prevEndDate != "" {
+			var prevTasksCompleted int64
+			if err := db.DB.Model(&models.Task{}).
+				Select("COUNT(*)").
+				Where("user_id = ? AND completed_at BETWEEN ? AND ?", userId, prevStartDate, prevEndDate).
+				Scan(&prevTasksCompleted).Error; err != nil {
+				c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get completed tasks!"})
+				return
+			}
+			change := tasksCompleted - prevTasksCompleted
+			tasksCompletedChange = &change
+		}
+	} else {
+		if err := db.DB.Model(&models.TaskSession{}).
+			Where("user_id = ?", userId).
+			Select("COALESCE(SUM(duration),0)").
+			Scan(&totalFocusTime).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get focus time!"})
+			return
+		}
+		if err := db.DB.Model(&models.Task{}).
+			Select("COUNT(*)").
+			Where("user_id = ? AND completed_at IS NOT NULL", userId).
+			Scan(&tasksCompleted).Error; err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to get completed tasks!"})
+			return
+		}
 	}
 
 	c.JSON(http.StatusOK, gin.H{
