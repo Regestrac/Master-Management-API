5e923ed36685d4f8cea6e9844ef0b253fbaef7c0
Sojo C Johny <sojocjohny37@gmail.com>
2025-10-22 11:35:51 +0530

    Implemented support for multiple origins in cors middleware.

--- internal/middleware/cors.go
+++ internal/middleware/cors.go
@@ -6,13 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+var allowedOrigins = map[string]bool{
+	"http://localhost:5173": true,
+	// "http://localhost:4173": true,
+	// "http://10.13.18.154:5173": true,
+}
+
 func CORSMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:5173")
-		// c.Writer.Header().Set("Access-Control-Allow-Origin", "http://10.13.18.154:5173")
-		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		origin := c.Request.Header.Get("Origin")
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+			c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization")
+			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+		}
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(http.StatusNoContent)
