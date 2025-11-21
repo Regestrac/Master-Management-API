8a7df967ebf8579fd393d3857e5c024515ccc2b6
Sojo C Johny <sojocjohny37@gmail.com>
2025-11-21 10:31:24 +0530

    Updated env loading configuration to support render hosting.

--- cmd/config/config.go
+++ cmd/config/config.go
@@ -2,14 +2,17 @@ package config
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load("../../.env")
-
-	if err != nil {
-		log.Fatal("Error loading .env file: " + err.Error())
+	// Only load .env when running locally
+	if os.Getenv("RENDER") == "" {
+		err := godotenv.Load("../../.env")
+		if err != nil {
+			log.Println("No .env file found, using environment variables")
+		}
 	}
 }
