eea5be5e7af4893daf312a4d261654e7dade563b
Sojo C Johny <sojocjohny37@gmail.com>
2026-06-09 11:44:07 +0530

    Added index.go file to support vercel deployment

--- api/index.go
+++ api/index.go
@@ -1,13 +1,17 @@
-package main
+package api
 
 import (
 	"log"
+	"net/http"
+
 	"master-management-api/cmd/config"
 	"master-management-api/internal/db"
 	"master-management-api/internal/models"
 	"master-management-api/internal/routes"
 	"master-management-api/pkg/ai"
 )
+
+var app http.Handler
 
 func init() {
 	config.LoadEnv()
@@ -16,9 +20,7 @@ func init() {
 	if err := ai.Init(); err != nil {
 		log.Fatalf("Failed to initialize Gemini: %v", err)
 	}
-}
 
-func main() {
 	log.Println("Starting Migration...")
 	if err := db.DB.AutoMigrate(
 		&models.User{},
@@ -31,10 +33,13 @@ func main() {
 		&models.UserSettings{},
 		&models.TaskSession{},
 	); err != nil {
-		log.Fatal("Failed to migrate:", err)
+		log.Printf("Failed to migrate: %v", err)
 	}
 	log.Println("Migration complete.")
 
-	routes.SetupVercelRouter()
-	routes.SetupRouter()
+	app = routes.SetupVercelRouter()
+}
+
+func Handler(w http.ResponseWriter, r *http.Request) {
+	app.ServeHTTP(w, r)
 }
